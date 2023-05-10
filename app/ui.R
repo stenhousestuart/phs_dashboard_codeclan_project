ddea7c078e8237c22f254fe69d508cfb350b6b2d
kirstyand <122488788+kirstyand@users.noreply.github.com>
2023-05-10 10:55:35 +0100

    changes geo tab layout

--- app/ui.R
+++ app/ui.R
@@ -1,19 +1,19 @@
 ui <- fluidPage(
   titlePanel("Change me!"),
   tabsetPanel(
-
+    
     tabPanel(
       title = ".README",
-
+      
       fluidRow(
         tags$br(),
         print("This is space for us to put some introductory text - explaining the
               purpose of the app, a short description of each tab along with details and rational
               behind any assumptions/decisions.")
       )
-
+      
     ),
-
+    
     tabPanel(
       title = "Stats",
       tags$br(),
@@ -24,24 +24,24 @@ ui <- fluidPage(
       tags$br(),
       fluidRow(
         selectInput(inputId = "stats_year_input",
-                     label = "Select Year:",
-                     choices = c("2017", "2018", "2019", "2020", "2021", "2022")
-      ),
+                    label = "Select Year:",
+                    choices = c("2017", "2018", "2019", "2020", "2021", "2022")
+        ),
       ),    
       
       fluidRow(
         dataTableOutput("stats_table_output")
       ),
       
-
+      
     ),
     tabPanel(
       title = "Temporal",
       fluidRow(
-               radioButtons(inputId = "temporal_plot_type_input",
-                            label = "View:",
-                            choices = c("Total Number of Admissions", "Mean Length of Stay")
-                            )
+        radioButtons(inputId = "temporal_plot_type_input",
+                     label = "View:",
+                     choices = c("Total Number of Admissions", "Mean Length of Stay")
+        )
       ),
       fluidRow(
         column(width = 6,
@@ -61,10 +61,10 @@ ui <- fluidPage(
         actionButton(inputId = "update_temporal",
                      label = "Update dashboard")
       ),
-
+      
       plotOutput("temporal_output"),
-
-
+      
+      
       fluidRow(
         print("This is space for us to put some analysis relating to the results of each graph - but for now it is just gonna have rubbish in it.
             This would be where some stats go, but good luck interpretting these nonsense graphs")
@@ -74,15 +74,16 @@ ui <- fluidPage(
     # will probably need more human readable choice names
     tabPanel(
       title = "Geographical",
-
+      
       column(width = 6,
+             offset = 1,
              pickerInput(inputId = "health_board_input_geo",
                          label = "Select Health Board",
                          choices = geo_healthboard_choice,
-                        selected = "All of Scotland",
-                       pickerOptions(actionsBox = TRUE),
-                        multiple = TRUE),
-                        
+                         selected = "All of Scotland",
+                         pickerOptions(actionsBox = TRUE),
+                         multiple = TRUE),
+             
       ),
       # column(width = 6,
       #        selectInput(inputId = "year_input_geo",
@@ -100,6 +101,7 @@ ui <- fluidPage(
       ),
       fluidRow(
         column(width = 4,
+               offset = 1,
                selectInput(inputId = "year_input_geo",
                            label = "Select Year",
                            choices = geo_year_choice,
@@ -111,13 +113,16 @@ ui <- fluidPage(
                            choices = geo_quarter_choice,
                            selected = "Q1")
         ),
-        column(width = 4,
+        column(width = 2,
                actionButton(inputId = "update_geo_date",
-                              label = "Update dashboard"))
-    ),
-    fluidRow(
-      leafletOutput("geo_map_output")
-    ),
+                            label = "Update dashboard"))
+      ),
+      fluidRow(
+        column(offset = 1,
+               width = 10,
+               leafletOutput("geo_map_output")
+        ),
+      ),
     ),
     
     tabPanel(
@@ -151,32 +156,32 @@ ui <- fluidPage(
         print("Please note that in the dataset used to produce these visualisations, there were a number of NA values for length of stay - 
         These have been dropped, and therefore may affect results.")
       ),
-),
-
+    ),
+    
     # using age for the demographic for testing purposes
     tabPanel(
       title = "Demographics",
       tabsetPanel(
         tabPanel(
           title = "Age",
-
+          
           fluidRow(
             tags$br(),
             column(width = 4,
-
+                   
                    selectInput(inputId = "age_input",
                                label = "Select Age Range(s) To Compare:",
                                choices = age_choice,
                                multiple = TRUE,
                                selected = "All ages")
             ),
-
+            
             column(width = 4,
                    radioButtons(inputId = "age_plot_type_input",
                                 label = "View:",
                                 choices = c("Mean No. of Admissions", "Mean Length of Stay"))
             ),
-
+            
             column(width = 4,
                    actionButton(inputId = "update_demo_age",
                                 label = "Update Dashboard")
@@ -185,95 +190,95 @@ ui <- fluidPage(
           fluidRow(
             plotOutput("demo_age_output")
           ),
-
+          
           fluidRow(
             print("This is space for us to put some analysis relating to the results of each graph - but for now it is just gonna have rubbish in it.
             This would be where some stats go, but good luck interpretting these nonsense graphs")
-
+            
           )
         ),
-
+        
         tabPanel(
           title = "Deprivation",
-
+          
           fluidRow(
             tags$br(),
             column(width = 4,
-
+                   
                    selectInput(inputId = "deprivation_input",
                                label = "Select Deprivation Categoris To Compare:",
                                choices = deprivation_choice,
                                multiple = TRUE)
             ),
-
+            
             column(width = 4,
                    radioButtons(inputId = "deprivation_plot_type_input",
                                 label = "View:",
                                 choices = c("Mean No. of Admissions", "Mean Length of Stay"))
             ),
-
+            
             column(width = 4,
                    actionButton(inputId = "update_demo_deprivation",
                                 label = "Update Dashboard")
             )
           ),
-
+          
           fluidRow(
             plotOutput("demo_deprivation_output")
           ),
-
+          
           fluidRow(
             print("The above plots use the Scottish Index of Multiple Deprivation, where SIMD 1
                   is considered the most deprived and SIMD 5 being least deprived.")
-
+            
           ),
         ),
-
+        
         tabPanel(
           title = "Gender & Age",
-
+          
           fluidRow(
             tags$br(),
             column(width = 4,
-
+                   
                    selectInput(inputId = "gender_age_input",
                                label = "Select Age Range(s) To Compare:",
                                choices = age_choice,
                                multiple = TRUE),
             ),
-
+            
             column(width = 4,
                    radioButtons(inputId = "age_gender_plot_type_input",
                                 label = "View:",
                                 choices = c("No. of Admissions", "Average Length of Stay")),
             ),
-
-
+            
+            
             column(width = 4,
                    actionButton(inputId = "update_demo_gender_age",
                                 label = "Update Dashboard"),
             ),
           ),
-
+          
           fluidRow(
             plotOutput("demo_age_gender_output"),
           ),
-
+          
           fluidRow(
             print("Only years where complete data is available have been included.")
-
+            
           ),
         ),
       ),
     ),
   ),
-    # WARNING: THIS MUST BE OUTSIDE tabSetPanel!
-    # Testing a "footer" that mentions where data was taken from
-    # Will need to alter link and title for actual dataset used
-    # Could be shifted into individual tabs if we end up using different datasets for different outputs
-    hr(),
-    print("Data taken from "),
-    tags$a(href = "https://www.opendata.nhs.scot/dataset/covid-19-wider-impacts-hospital-admissions/resource/f8f3a435-1925-4c5a-b2e8-e58fdacf04bb",
-           "Public health Scotland")
-
+  # WARNING: THIS MUST BE OUTSIDE tabSetPanel!
+  # Testing a "footer" that mentions where data was taken from
+  # Will need to alter link and title for actual dataset used
+  # Could be shifted into individual tabs if we end up using different datasets for different outputs
+  hr(),
+  print("Data taken from "),
+  tags$a(href = "https://www.opendata.nhs.scot/dataset/covid-19-wider-impacts-hospital-admissions/resource/f8f3a435-1925-4c5a-b2e8-e58fdacf04bb",
+         "Public health Scotland")
+  
 )
